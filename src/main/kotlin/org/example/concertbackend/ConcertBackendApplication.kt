db40c861a786aafbdf753572f436f7cf42813c77
yongckim <zxcv9203@naver.com>
2024-10-06 18:20:31 +0900

    :tada: init : 프로젝트 초기화

---
+++ src/main/kotlin/org/example/concertbackend/ConcertBackendApplication.kt
@@ -0,0 +1,11 @@
+package org.example.concertbackend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ConcertBackendApplication
+
+fun main(args: Array<String>) {
+    runApplication<ConcertBackendApplication>(*args)
+}
