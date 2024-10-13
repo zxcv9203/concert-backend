dcc69dd14e26cad57a9bd3d66c938728ecb8fe19
yongckim <zxcv9203@naver.com>
2024-10-14 00:20:16 +0900

    :wrench: config : 기본 데이터베이스 설정 및 스키마, 더미 데이터 추가

---
+++ src/main/resources/db/data.sql
@@ -0,0 +1,3 @@
+INSERT INTO users (name, `created_at`, `updated_at`) VALUES ('user1', now(), now())
+INSERT INTO users (name, `created_at`, `updated_at`) VALUES ('user2', now(), now())
+INSERT INTO users (name, `created_at`, `updated_at`) VALUES ('user3', now(), now())
